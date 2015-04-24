3b2b8ff451111133104d4a7ef2cbf42e27365406
Blake Mizerany <blake.mizerany@gmail.com>
2015-04-24 13:13:16 -0700

    mitm: add response inspection to example

--- example_test.go
+++ example_test.go
@@ -7,6 +7,17 @@ import (
 	"testing"
 )
 
+type codeRecorder struct {
+	http.ResponseWriter
+
+	code int
+}
+
+func (w *codeRecorder) WriteHeader(code int) {
+	w.ResponseWriter.WriteHeader(code)
+	w.code = code
+}
+
 func ExampleProxy(t *testing.T) {
 	ca, err := loadCA()
 	if err != nil {
@@ -17,8 +28,10 @@ func ExampleProxy(t *testing.T) {
 		CA: &ca,
 		Wrap: func(upstream http.Handler) http.Handler {
 			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				cr := &codeRecorder{ResponseWriter: w}
 				log.Println("Got Content-Type:", r.Header.Get("Content-Type"))
-				upstream.ServeHTTP(w, r)
+				upstream.ServeHTTP(cr, r)
+				log.Println("Got Status:", cr.code)
 			})
 		},
 	}
