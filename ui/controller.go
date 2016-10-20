09d729bddf2f21ba3e58a8e02f8956941a28cc7f
RobClap <robclap8@gmail.com>
2016-10-19 21:14:38 -0700

    Improved data structure

--- ui/controller.go
+++ ui/controller.go
@@ -1,6 +1,9 @@
 package ui
 
-import "sync"
+import (
+	"log"
+	"sync"
+)
 
 //only listen for localhost
 type Command struct {
@@ -8,22 +11,56 @@ type Command struct {
 	Args    map[string]string
 }
 
-//FIXME This is not a good solution. Implement something that a component can unsubsctibe from
-var subScriptions map[string][]chan<- Command
-var subsMutex sync.Mutex
+type Args map[string]string
+
+var subScriptions map[string]map[int64]Subscription
+var subsMutex sync.RWMutex
+var subsCounter int64
 var ioChan chan Command
 
 func init() {
-	subScriptions = make(map[string][]chan<- Command)
+	subScriptions = make(map[string]map[int64]Subscription)
 	ioChan = make(chan Command)
+}
+
+type Subscription struct {
+	id      int64
+	channel chan Command
+}
+
+func (s *Subscription) Read() Command {
+	return <-s.channel
 }
 
 //Unless you are sure the out channel will be constantly read, it is strongly
 //suggested to create a buffered channel
-func SubScribe(channel string, out chan<- Command) {
+func SubScribe(channel string) Subscription {
 	subsMutex.Lock()
-	defer subsMutex.Unlock()
-	subScriptions[channel] = append(subScriptions[channel], out)
+	subsCounter++
+	pipe := make(chan Command, 20) //TODO this is arbitrary, give a meaning to this number
+	out := Subscription{id: subsCounter, channel: pipe}
+	if subScriptions[channel] == nil {
+		subScriptions[channel] = make(map[int64]Subscription)
+	}
+	subScriptions[channel][subsCounter] = out
+	subsMutex.Unlock()
+	return out
+}
+
+func UnSubscribe(s *Subscription) {
+	subsMutex.RLock()
+	defer subsMutex.RUnlock()
+	for _, channelSubs := range subScriptions {
+		sub, ok := channelSubs[s.id]
+		if ok {
+			subsMutex.Lock()
+			close(sub.channel)
+			delete(channelSubs, s.id)
+			subsMutex.Unlock()
+			return
+		}
+	}
+	log.Println("Subscription not found")
 }
 
 func Send(c Command) {
@@ -32,10 +69,10 @@ func Send(c Command) {
 
 func MainLoop() {
 	for cmd := range ioChan {
-		subsMutex.Lock()
+		subsMutex.RLock()
 		for _, out := range subScriptions[cmd.Channel] {
-			out <- cmd
+			out.channel <- cmd
 		}
-		subsMutex.Unlock()
+		subsMutex.RUnlock()
 	}
 }
