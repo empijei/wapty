25530d3826545829d43e85d6cb19697cefd7c7c4
empijei <empijei@gmail.com>
2017-06-13 17:26:16 +0200

    Removed unused files, started testing repeat/queues

--- repeat/queues_test.go
+++ repeat/queues_test.go
@@ -1,12 +1,38 @@
 package repeat
 
-//TODO
-/*
+import (
+	"testing"
+
+	"github.com/empijei/wapty/ui"
+	"github.com/empijei/wapty/ui/apis"
+)
+
+type MockSubscription struct {
+	Id        int64
+	Channel   string
+	DataCh    chan apis.Command
+	SentStuff []apis.Command
+}
+
+func (s *MockSubscription) Receive() apis.Command {
+	return <-s.DataCh
+}
+func (s *MockSubscription) RecChannel() <-chan apis.Command {
+	return s.DataCh
+}
+
+func (s *MockSubscription) Send(c apis.Command) {
+	s.SentStuff = append(s.SentStuff, c)
+}
+
 func TestHandleGo(t *testing.T) {
+	uirbak := uiRepeater
+	defer func() {
+		uiRepeater = uirbak
+	}()
 	testSubc := make(chan apis.Command)
-	testSub := ui.Subscription{
+	testSub := &MockSubscription{
 		DataChannel: testchan,
 	}
 	testUI := ui.UI{}
 }
-*/
