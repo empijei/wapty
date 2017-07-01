50079c4d6c209731b22d170735eb97754fc6a81b
silverweed <silverweed1991@gmail.com>
2017-07-01 14:10:52 +0200

    add header skip to parse XML 1.1

--- mocksy/server.go
+++ mocksy/server.go
@@ -30,10 +30,18 @@ func StartServer(port string) error {
 
 func loadResponseHistory() error {
 	// Import data (TODO: should not be hardcoded here, also should check errors)
-	fname := "test.xml"
+	fname := "burp.xml"
 	file, err := os.Open(fname)
 	if err != nil {
 		return fmt.Errorf("File not found: %s", fname)
+	}
+	// Go refuses to parse any XML whose version is != "1.0". Burp sometimes
+	// declares XML 1.1, albeit it uses no 1.1-only features, so we trick
+	// the XML parser into parsing our "invalid" XML by skipping the XML header.
+	buf := make([]byte, len(`<?xml version="1.x"?>`))
+	if n, err := file.Read(buf); err == nil && n == len(buf) {
+	} else {
+		return fmt.Errorf("mocksy: error importing data: header skip failed.\n")
 	}
 	items, err := BurpImport(file)
 	if err != nil {
