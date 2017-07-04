62ff6aedfca2c77e5d8e4f1f0f94a9eee33291bc
AnnaOpss <hi@anna.so>
2017-07-04 16:19:35 +0200

    Added some test

--- decode/b16_test.go
+++ decode/b16_test.go
@@ -1,6 +1,7 @@
 package decode
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -11,6 +12,11 @@ var Base16Test = []struct {
 }{
 	{
 		"666F6F626172",
+		"foobar",
+		true,
+	},
+	{
+		"666f6f626172",
 		"foobar",
 		true,
 	},
@@ -44,6 +50,43 @@ var Base16Test = []struct {
 		genInvalid(1),
 		false,
 	},
+	{
+		"",
+		"",
+		true,
+	},
+}
+
+var Base16EncodeTest = []struct {
+	in   string
+	eOut string
+}{
+	{
+		"foobar",
+		"666F6F626172",
+	},
+	{
+		"fooba▶︎",
+		"666F6F6261E296B6EFB88E",
+	},
+	{
+		"",
+		"",
+	},
+}
+
+var Base16CheckTest = []struct {
+	in   string
+	eOut float64
+}{
+	{
+		"666F6F626172",
+		1,
+	},
+	{
+		"666F6F62617",
+		0.91,
+	},
 }
 
 func TestB16Decode(t *testing.T) {
@@ -57,4 +100,34 @@ func TestB16Decode(t *testing.T) {
 			t.Errorf("Expected printable: %v", tt.eIsPrint)
 		}
 	}
+}
+
+func TestB16Encode(t *testing.T) {
+	for _, tt := range Base16EncodeTest {
+		d := NewB16CodecC(tt.in)
+		out := d.Encode()
+		if strings.ToUpper(out) != tt.eOut {
+			t.Errorf("Expected encoded value: '%s' but got '%s'", tt.eOut, out)
+		}
+	}
+}
+
+func TestB16Check(t *testing.T) {
+	for _, tt := range Base16CheckTest {
+		d := NewB16CodecC(tt.in)
+		out := d.Check()
+		if CompareFloat(out, tt.eOut, 0.1) != 0 {
+			t.Errorf("Expected check value of '%f' but got '%f'", tt.eOut, out)
+		}
+	}
+}
+
+func CompareFloat(a, b float64, tolerance float64) int {
+	if a < b-tolerance {
+		return 1
+	}
+	if b > a+tolerance {
+		return -1
+	}
+	return 0
 }
