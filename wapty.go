09ec0a7277aabadfde4a10fa638c69d4adcbcfae
silverweed <silverweed1991@gmail.com>
2017-07-08 12:45:37 +0200

    add fallback Version and Commit
    Currently, one can just `go build` wapty bypassing the makefile.
    In that case, Version and Commit are set to "Unknown" rather than
    the empty string.

--- wapty.go
+++ wapty.go
@@ -28,6 +28,13 @@ var commands = []struct {
 	{"proxy", proxyMain},
 	{"mocksy", mocksy.Main},
 	{"version", func() {
+		// Setup fallback version and commit in case wapty wasn't "properly" compiled
+		if len(Version) == 0 {
+			Version = "Unknown"
+		}
+		if len(Commit) == 0 {
+			Commit = "Unknown"
+		}
 		fmt.Printf("Version: %s\nCommit: %s\n", Version, Commit)
 	}},
 }
