8b806c99a218690e367231332ab43dda19bcd8bd
silverweed <silverweed1991@gmail.com>
2017-08-14 12:25:03 +0200

    fix decode test, restructure mainstandalone a bit

--- decode/main.go
+++ decode/main.go
@@ -11,43 +11,60 @@ import (
 // `wapty decode`. This behaves as a main and expects the "decode" parameter to
 // be removed from os.Args.
 func MainStandalone(args ...string) {
-
-	// FIXME: argument validation should be separated from encoding/decoding
 	buf := takeInput(args)
 	sequence := strings.Split(flagCodeclist, ",")
-	for i, codec := range sequence {
-		var c CodecC
-		var codecNames []string
-		if codec == "smart" {
-			if flagEncode {
-				fmt.Fprintf(os.Stderr, "Cannot 'smart' encode, please specify a codec")
-				os.Exit(2)
-			}
-			c = SmartDecode(buf)
-		} else {
-			for _, cc := range codecs {
-				if cc.name == codec {
-					c = cc.codecCons(buf)
-				}
-				codecNames = append(codecNames, cc.name)
-			}
-			if c == nil {
-				fmt.Fprintf(os.Stderr, "Codec not found: %s. Supported codecs are: %s\n", codec, strings.Join(codecNames, ", "))
-				os.Exit(2)
-			}
-		}
-		fmt.Fprintf(os.Stderr, "Codec: %s\n", c.Name())
-		if flagEncode {
-			buf = c.Encode()
-		} else {
-			buf = c.Decode()
-		}
+	for _, codec := range sequence {
 		//This is to avoid printing twice the final result
-		if i < len(sequence)-2 {
-			fmt.Fprintln(os.Stderr, buf)
+		//if i < len(sequence)-2 {
+		//fmt.Fprintln(os.Stderr, buf)
+		//}
+		if out, codecUsed, err := DecodeEncode(buf, flagEncode, codec); err == nil {
+			fmt.Printf("Codec: %s\n%s\n", codecUsed, out)
+			buf = out
+		} else {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(2)
 		}
 	}
-	fmt.Printf(buf)
+}
+
+// DecodeEncode takes an input string `buf` and decodes/encodes it (depending on the
+// `encode` parameter) with the given `codec`. It returns the encoded/decoded string
+// or an error if the process failed.
+func DecodeEncode(buf string, encode bool, codec string) (out string, codecUsed string, err error) {
+
+	// Build list of available codecs
+	var codecNames []string
+	for _, cc := range codecs {
+		codecNames = append(codecNames, cc.name)
+	}
+	codecNamesStr := strings.Join(codecNames, ", ")
+
+	var c CodecC
+	if codec == "smart" {
+		if encode {
+			err = fmt.Errorf("Cannot 'smart' encode, please specify a codec")
+			return
+		}
+		c = SmartDecode(buf)
+	} else {
+		for _, cc := range codecs {
+			if cc.name == codec {
+				c = cc.codecCons(buf)
+			}
+		}
+		if c == nil {
+			err = fmt.Errorf("Codec not found: '%s'. Supported codecs are: %s\n", codec, codecNamesStr)
+			return
+		}
+	}
+	codecUsed = c.Name()
+	if encode {
+		out = c.Encode()
+	} else {
+		out = c.Decode()
+	}
+	return
 }
 
 func takeInput(args []string) string {
--- decode/main_test.go
+++ decode/main_test.go
@@ -1,75 +1,203 @@
 package decode
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 )
 
-var CodecPayloadTest = []struct {
-	cFlag   string
-	in      string
-	eOut    string
-	success bool
+type expectation int
+
+const (
+	expectGood expectation = iota // expect a good result with no errors
+	expectBad                     // expect a bad result with no errors
+	expectErr                     // expect errors
+)
+
+var testDecodeData = []struct {
+	cFlag  string
+	in     string
+	eOut   string
+	expect expectation
 }{
 	{
 		"b16",
 		"666F6F626172",
 		"foobar",
-		true,
+		expectGood,
 	},
 	{
 		"b16,b64",
 		"5a6d3976",
 		"foo",
-		true,
+		expectGood,
 	},
 	{
 		",,",
 		"5a6d3976",
 		"",
-		false,
+		expectErr,
 	},
 	{
 		"",
 		"5a6d3976",
 		"",
-		false,
+		expectErr,
 	},
 	{
 		"smart,smart",
 		"5a6d3976",
 		"foo",
-		true,
+		expectGood,
 	},
 }
 
-//func init() {
-//RegisterFlagStandalone()
-//}
-
-func TestMainStandalone(t *testing.T) {
-	/* TODO: rewrite this in a sane manner
-	if os.Getenv("BE_MAIN") == "1" {
-		MainStandalone()
-		return
+func TestDecode(t *testing.T) {
+	succFailStr := map[bool]string{
+		true:  "good result",
+		false: "bad result",
 	}
-
-	for _, tt := range CodecPayloadTest {
-		cmd := exec.Command(os.Args[0], "-test.run=TestMainStandalone", "-codec", tt.cFlag, tt.in)
-		cmd.Env = append(os.Environ(), "BE_MAIN=1")
-
-		var out bytes.Buffer
-		cmd.Stdout = &out
-		err := cmd.Run()
-
-		res := string(out.Bytes())
-
-		if e, ok := err.(*exec.ExitError); ok && e.Success() != tt.success {
-			t.Errorf("Expected exit code: %v, but got %v", tt.success, e.Success())
+	for _, test := range testDecodeData {
+		wasErrRaised := false
+		buf := test.in
+		for _, codec := range strings.Split(test.cFlag, ",") {
+			var err error
+			buf, _, err = DecodeEncode(buf, false, codec)
+			if err != nil {
+				if test.expect != expectErr {
+					t.Errorf("On call DecodeEncode('%s', false, '%s'):\n", buf, codec)
+					t.Fatalf("Unexpected error while decoding: %s", err.Error())
+				}
+				wasErrRaised = true
+			}
 		}
-
-		if !strings.HasPrefix(res, tt.eOut) {
-			t.Errorf("Expected decoded value: %s, but got '%s'", tt.eOut, res)
+		if test.expect == expectErr {
+			if !wasErrRaised {
+				t.Errorf("Expected error on test decode(in='%s', codecs='%s') -> '%s', but got none",
+					test.in, test.cFlag, test.eOut)
+			}
+		} else if (buf == test.eOut) != (test.expect == expectGood) {
+			t.Errorf("Expected %s on test decode(in='%s', codecs='%s') -> '%s', but got %s",
+				succFailStr[test.expect == expectGood],
+				test.in,
+				test.cFlag,
+				test.eOut,
+				succFailStr[buf == test.eOut],
+			)
+			t.Errorf("\n\tout: '%s'\n\n\texpected: '%s'\n", buf, test.eOut)
 		}
 	}
-	*/
+}
+
+var testGoodCodecData = []struct {
+	in         string
+	codecsUsed []string
+}{
+	{
+		"Zm9vYmFyIC1uCg==",
+		[]string{"b64"},
+	},
+	{
+		"5a6d3976",
+		[]string{"b16", "b64"},
+	},
+}
+
+func TestGoodCodec(t *testing.T) {
+	for _, test := range testGoodCodecData {
+		buf := test.in
+		var codecUsed string
+		var err error
+		for _, expectedCodec := range test.codecsUsed {
+			prevBuf := buf
+			buf, codecUsed, err = DecodeEncode(buf, false, "smart")
+			if err != nil {
+				t.Errorf("On call DecodeEncode('%s', false, 'smart'):\n", buf)
+				t.Fatalf("Unexpected error while decoding: %s", err.Error())
+			}
+			if codecUsed != expectedCodec {
+				t.Errorf("Decoding '%s': expected codec '%s' but got '%s'",
+					prevBuf, expectedCodec, codecUsed)
+			}
+		}
+	}
+}
+
+func TestDecodeStdin(t *testing.T) {
+	// TODO
+}
+
+var testEncodeData = []struct {
+	in         string
+	codecsUsed []string
+	out        string
+}{
+	{
+		"foobar",
+		[]string{"b64"},
+		"Zm9vYmFy",
+	},
+	{
+		"asdasd",
+		[]string{"b16"},
+		"617364617364",
+	},
+	{
+		"wapty",
+		[]string{"b32"},
+		"O5QXA5DZ",
+	},
+	{
+		"asdasd",
+		[]string{"b16", "b32", "b64"},
+		"R1lZVE9NWldHUTNEQ05aVEdZMkE9PT09",
+	},
+	{
+		"asdasd",
+		[]string{"b16", "b16", "b16"},
+		"333633313337333333363334333633313337333333363334",
+	},
+	{
+		"asdasd",
+		[]string{"b32", "b32", "b32"},
+		"JJLEIRSVKYZEUTCGI5DESVCLKJEFKNSUGJIEUNKIKU6T2PJ5HU6Q====",
+	},
+	{
+		"asdasd",
+		[]string{"b64", "b64", "b64"},
+		"V1ZoT2ExbFlUbXM9",
+	},
+}
+
+func TestEncode(t *testing.T) {
+	type step struct {
+		codec string
+		out   string
+	}
+	for _, test := range testEncodeData {
+		buf := test.in
+		var err error
+		var intermediate []step
+		for _, codecUsed := range test.codecsUsed {
+			buf, _, err = DecodeEncode(buf, true, codecUsed)
+			intermediate = append(intermediate, step{codecUsed, buf})
+			if err != nil {
+				t.Errorf("On call DecodeEncode('%s', true, '%s'):\n", buf, codecUsed)
+				t.Fatalf("Unexpected error while encoding: %s", err.Error())
+			}
+		}
+		if buf != test.out {
+			t.Errorf("Encoding '%s' with %s:\n\n\texpected\n\t\t'%s'\n\tbut got\n\t\t'%s'",
+				test.in, strings.Join(test.codecsUsed, ","), test.out, buf)
+			s := "Intermediate steps:\n"
+			for _, stp := range intermediate {
+				s += fmt.Sprintf("\tcodec: %s\n\tresult: %s\n", stp.codec, stp.out)
+			}
+			t.Errorf(s)
+		}
+	}
+}
+
+func TestEncodeStdin(t *testing.T) {
+	// TODO
 }
