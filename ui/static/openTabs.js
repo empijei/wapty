9b1bc0edbc68749e015fc45a006c93b56288fc37
AnnaOpss <hi@anna.so>
2018-05-12 12:21:11 -0700

    Fixed adding new tabs

--- ui/static/openTabs.js
+++ ui/static/openTabs.js
@@ -2,24 +2,23 @@
 var oReq = new XMLHttpRequest();
 oReq.open("GET", "/modules/enabled_modules.json", true);
 oReq.onload = function () { 
-				console.log(this.responseText); 
-				var enabledModules = JSON.parse(this.responseText);
+	console.log(this.responseText); 
+	var enabledModules = JSON.parse(this.responseText);
 
-				var tabs = document.getElementById('tab_modules');
-				console.log(tabs)
-				tabs.innerHTML = '';
+	var tabs = document.getElementById('tab_modules');
+	tabs.innerHTML = '';
 
-				for (var module in enabledModules) {
-								var li = document.createElement('li');
-								li.classList.add("tablinks");
-								tabs.appendChild(li);
+	for (var module in enabledModules) {
+		var li = document.createElement('li');
+		li.classList.add("tablinks");
+		tabs.appendChild(li);
 
-								var anchor = document.createElement('a');
-								anchor.addEventListener('click', handler, false);
-								var text = document.createTextNode(enabledModules[module]);
-								anchor.appendChild(text);
-								tabs.appendChild(anchor);
-				}
+		var anchor = document.createElement('a');
+		anchor.addEventListener('click', handler, false);
+		var text = document.createTextNode(enabledModules[module]);
+		anchor.appendChild(text);
+		tabs.appendChild(anchor);
+	}
 };
 oReq.send(null);
 
@@ -27,50 +26,43 @@ oReq.send(null);
 var req = new XMLHttpRequest();
 req.open('GET', 'modules/Proxy/index.html', true);
 req.onload = function () {
-				var workspace = document.getElementById('workspace');
-				workspace.innerHTML = this.responseText;
+	var workspace = document.getElementById('workspace');
+	workspace.innerHTML = this.responseText;
 }
 req.send(null);
 
 //Loading tab content
 function handler(e, data) {
-				var module_name = e.toElement.innerHTML;
+	var module_name = e.toElement.innerHTML;
 
-				var req = new XMLHttpRequest();
-				req.open('GET', 'modules/' + module_name + '/index.html', true);
-				req.onload = function () {
-								var workspace = document.getElementById('workspace');
-								workspace.innerHTML = this.responseText;
-				}
-				req.send(null);
+	var req = new XMLHttpRequest();
+	req.open('GET', 'modules/' + module_name + '/index.html', true);
+	req.onload = function () {
+		var workspace = document.getElementById('workspace');
+		workspace.innerHTML = this.responseText;
+	}
+	req.send(null);
 }
 
 function openTab(evt, tabTitle) {
-				var i, tabcontent, tablinks;
-				tabcontent = document.getElementsByClassName("tabcontent");
-				for (i = 0; i < tabcontent.length; i++) {
-								tabcontent[i].style.display = "none";
-				}
-				tablinks = document.getElementsByClassName("tablinks");
-				for (i = 0; i < tablinks.length; i++) {
-								tablinks[i].className = tablinks[i].className.replace(" is-active", "");
-				}
-				document.getElementById(tabTitle).style.display = "block";
-				evt.path[1].className += " is-active";
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+	}
+	console.log(document.getElementById(tabTitle));
+	document.getElementById(tabTitle).style.display = "block";
+	evt.path[1].className += " is-active";
 }
 
 function createTab(evt, tabTitle) {
-				var li = document.createElement("LI");
-				li.classList.add("tablinks");
-				var a = document.createElement("A");
-				a.addEventListener("click", "openTab(event, 'Name')");
-				var name = document.createTextNode(tabTitle);
-				a.appendChild(name);
-				li.appendChild(a);
-				document.getElementById("tabsTitle").appendChild(li);
+	var newtabTitle = document.getElementById('1title');
+	newtabTitle.insertAdjacentHTML('afterend', '<li class="tablinks"><a onclick="openTab(event, \'Name\')">Name</a></li>');
 
-				var div = document.createElement("DIV");
-				div.classList.add("tabcontent");
-				div.id = tabTitle;
-				document.getElementById("tabs").appendChild(div)
+	var newtabContent = document.getElementById('1');
+	newtabContent.insertAdjacentHTML('afterend', '<div class="tabcontent" id="Name"><textarea class="textarea" placeholder="New Tab" rows="30"></textarea></div>');
 }
