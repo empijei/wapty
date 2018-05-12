1441c1bcc7219bfd68cbed86cca0bed037f2b7e7
AnnaOpss <hi@anna.so>
2018-05-12 08:10:07 -0700

    Creating new tabs for repeater

--- ui/static/openTabs.js
+++ ui/static/openTabs.js
@@ -11,3 +11,19 @@ function openTab(evt, tabTitle) {
     document.getElementById(tabTitle).style.display = "block";
     evt.path[1].className += " is-active";
 }
+
+function createTab(evt, tabTitle) {
+				var li = document.createElement("LI");
+				li.classList.add("tablinks");
+				var a = document.createElement("A");
+				a.addEventListener("onClick", "openTab(event, 'Name')");
+				var name = document.createTextNode(tabTitle);
+				a.appendChild(name);
+				li.appendChild(a);
+				document.getElementById("tabsTitle").appendChild(li);
+
+				var div = document.createElement("DIV");
+    div.classList.add("tabcontent");
+    div.id = tabTitle;
+		  document.getElementById("tabs").appendChild(div)
+}
