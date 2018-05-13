58f45a8511bf46a6ae94ea9fb22ab1f486c00b57
AnnaOpss <hi@anna.so>
2018-05-12 17:05:52 -0700

    Repeater: added the close tab button

--- ui/static/openTabs.js
+++ ui/static/openTabs.js
@@ -2,7 +2,7 @@
 var oReq = new XMLHttpRequest();
 oReq.open("GET", "/modules/enabled_modules.json", true);
 oReq.onload = function () { 
-	console.log(this.responseText); 
+	//console.log(this.responseText); 
 	var enabledModules = JSON.parse(this.responseText);
 
 	var tabs = document.getElementById('tab_modules');
@@ -11,13 +11,16 @@ oReq.onload = function () {
 	for (var module in enabledModules) {
 		var li = document.createElement('li');
 		li.classList.add("tablinks");
+		if (enabledModules[module] == "Proxy") {
+			li.classList.add("is-active");
+		}
 		tabs.appendChild(li);
 
 		var anchor = document.createElement('a');
 		anchor.addEventListener('click', handler, false);
 		var text = document.createTextNode(enabledModules[module]);
 		anchor.appendChild(text);
-		tabs.appendChild(anchor);
+		li.appendChild(anchor);
 	}
 };
 oReq.send(null);
@@ -34,6 +37,11 @@ req.send(null);
 //Loading tab content
 function handler(e, data) {
 	var module_name = e.toElement.innerHTML;
+	//Removing is-active attributes
+	var actTab = document.getElementsByClassName("is-active");
+	actTab[0].classList.remove("is-active");
+	//Adding is-active class to selected tab
+	e.path[1].classList.add("is-active");
 
 	var req = new XMLHttpRequest();
 	req.open('GET', 'modules/' + module_name + '/index.html', true);
@@ -54,15 +62,23 @@ function openTab(evt, tabTitle) {
 	for (i = 0; i < tablinks.length; i++) {
 		tablinks[i].className = tablinks[i].className.replace(" is-active", "");
 	}
-	console.log(document.getElementById(tabTitle));
+	//console.log(document.getElementById(tabTitle));
 	document.getElementById(tabTitle).style.display = "block";
 	evt.path[1].className += " is-active";
 }
 
 function createTab(evt, tabTitle) {
-	var newtabTitle = document.getElementById('1title');
-	newtabTitle.insertAdjacentHTML('afterend', '<li class="tablinks"><a onclick="openTab(event, \'Name\')">Name</a></li>');
+	//Adding a tab in the navbar
+	var newtabTitle = document.getElementById('titles');
+	newtabTitle.insertAdjacentHTML('beforeend', '<li class="tablinks"><a onclick="openTab(event, \'Name\')">Name <button class="delete is-small" onclick="closeTab(event)"></button></a></li>');
 
-	var newtabContent = document.getElementById('1');
-	newtabContent.insertAdjacentHTML('afterend', '<div class="tabcontent" id="Name"><textarea class="textarea" placeholder="New Tab" rows="30"></textarea></div>');
+	//Adding the content of the tab
+	//FIXME: all contents are shown at the same time
+	var newtabContent = document.getElementById('contents');
+	newtabContent.insertAdjacentHTML('beforeend', '<div class="tabcontent" id="Name"><textarea class="textarea" placeholder="New Tab" rows="30"></textarea></div>');
+}
+
+function closeTab(evt) {
+	evt.path[2].remove();
+	console.log(evt);
 }
